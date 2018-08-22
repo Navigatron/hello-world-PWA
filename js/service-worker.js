2dc9a7f7cf7a1b5edd9f0d0595d6371cfe23a4a2
Ethan Witherington <wyddindustries@gmail.com>
2018-08-22 18:43:01 -0400

    Service Worker now attempts to cache relative paths

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,15 +1,15 @@
 var cacheName = 'spinner';
 // I don't know to use relative or absolute paths here.
 var filesToCache = [
-    '/',
-    '/index.html',
-    '/css',
-    '/css/master.css',
-    '/icons',
-    '/icons/source.svg',
-    '/js',
-    '/js/app.js',
-    '/js/service-worker.js'
+    '../',
+    '../index.html',
+    '../css',
+    '../css/master.css',
+    '../icons',
+    '../icons/source.svg',
+    '../js',
+    '../js/app.js',
+    '../js/service-worker.js'
 ];
 
 /*
@@ -31,11 +31,11 @@ var filesToCache = [
 self.addEventListener(
     'install',
     function(e) {
-        console.log('[ServiceWorker] Install');
+        console.log('[service-worker] Install Event');
         e.waitUntil(
             caches.open(cacheName).then(
                 function(cache) {
-                    console.log('[ServiceWorker] Caching app shell');
+                    console.log('[service-worker] Caching app shell');
                     return cache.addAll(filesToCache);
                 }
             )
