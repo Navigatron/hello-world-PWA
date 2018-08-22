4e095518fb393a03fed682b91c3f751e4026c400
Ethan Witherington <wyddindustries@gmail.com>
2018-08-22 18:38:35 -0400

    Objective 3 to Testing Phase

--- js/app.js
+++ js/app.js
@@ -12,20 +12,38 @@ var spinning = false;
 var arrow;
 
 window.onload = function(){
+    // We are alive!
     console.log("Window Loaded");
+    // Get the arrow image for later reference
     arrow = document.getElementById('arrow');
+    // Make it spin when we click it
     arrow.addEventListener("click", spin);
+    // Make it spin when we touch it
     arrow.addEventListener("touchstart", function(e){
         e.preventDefault();
         spin();
     });
+    // Make the iPhone not do that zooming thing when you touch it a lot.
     arrow.addEventListener("touchend", function(e){
         e.preventDefault();
     });
+    // Let's see if we're in the iPhone app or not, just for fun.
     if(window.navigator.standalone){
         console.log("We are in Web-App Mode on an iPhone");
     }else{
         console.log("We are NOT in web-app mode on an iPhone.");
+    }
+    // Let's register a service worker so we can do this offline
+    if('serviceWorker' in navigator) {
+        console.log("[service-worker] Attempting to Register...");
+        navigator.serviceWorker.register("./js/service-worker.js").then(
+            function() {
+                console.log("[service-worker] Registered Successfully!");
+            },
+            function(){
+                console.log("[service-worker] Registration Failed.");
+            }
+        );
     }
 }
 
